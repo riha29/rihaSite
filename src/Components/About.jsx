4ee57f14d6fd806aa27696a2605ecfa98c78fb09
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-10-03 18:40:03 +0000

    changed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CSE student studying at Brac University. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -34,8 +34,8 @@ const skillsList = [
   "Web design",
   "User experience",
   "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Quality testing",
+  "Mobile interfaces",
   "Graphic design",
 ];
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,10 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+// import image from "../images/woman-with-tablet.jpg";
+import image from "../images/IMG_1921.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Riha is looking at the bookself and finding the right one";
 
 const Home = ({ name, title }) => {
   return (
